d7a296762e47aa6f628579f46d6d538f295cdad4
tormozz48 <tormozz48@gmail.com>
2017-01-10 23:00:42 +0300

    feat: Implement hermione entry point

---
+++ hermione.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const lib = require('./lib');
+
+module.exports = (hermione, options) => {
+    options = lib.parseConfig(options);
+
+    if (!options.enabled) {
+        return;
+    }
+
+    const events = hermione.events;
+    const stat = new lib.Stat();
+
+    hermione.on(events.SESSION_START, (wd, data) => stat.markStartBrowserTime(data.browserId));
+    hermione.on(events.SESSION_END, (wd, data) => stat.markEndBrowserTime(data.browserId));
+
+    hermione.on(events.TEST_FAIL, (data) => stat.addFailed(data.browserId));
+    hermione.on(events.TEST_PASS, (data) => stat.addPassed(data.browserId));
+    hermione.on(events.TEST_PENDING, (data) => stat.addSkipped(data.browserId));
+
+    hermione.on(events.RETRY, (data) => stat.addRetry(data.browserId));
+
+    hermione.on(events.RUNNER_END, () => lib.showReport(stat.getStatistic(), options));
+};
